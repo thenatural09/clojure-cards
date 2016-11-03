2c6193b186e7da37ba02319c8cfaa699a6a0ea26
Troy <tward4@tulane.edu>
2016-11-03 11:56:00 -0400

    Playing with cards in clojure

--- src/clojure_cards/core.clj
+++ src/clojure_cards/core.clj
@@ -1,6 +1,28 @@
 (ns clojure-cards.core
   (:gen-class))
 
+(def suits [:clubs :spades :hearts :diamonds])
+(def ranks (range 1 14))
+
+(defn create-deck[]
+  (set
+    (for [suit suits
+          rank ranks]
+        {:suit suit :rank rank})))
+
+(defn create-hands [deck]
+  (set
+    (for [c1 deck
+          c2 (disj deck c1)
+          c3 (disj deck c1 c2)
+          c4 (disj deck c1 c2 c3)]
+         #{c1 c2 c3 c4})))
+
+(defn flush? [hand])
+
 (defn -main [& args]
-  (println "Hello, world!"))
+  (let [deck (create-deck)
+        hands(create-hands deck)]
+    (println (count hands))))
+  
 
